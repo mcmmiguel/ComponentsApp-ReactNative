4706c43fbcb256ca510894dace35a7afd8df876e
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-17 21:24:42 -0600

    Feat. Fade In / Fade Out

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,13 +1,37 @@
 import React, { useRef } from 'react';
-import { View, StyleSheet, Animated } from 'react-native';
+import { View, StyleSheet, Animated, Button } from 'react-native';
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.5)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 1,
+                duration: 300,
+                useNativeDriver: true,
+            }
+        ).start(() => console.log('La animación terminó'));
+    };
+
+    const fadeOut = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true,
+            }
+        ).start();
+    };
 
     return (
         <View style={styles.mainContainer}>
             <Animated.View style={{ ...styles.purpleBox, opacity }} />
+            <Button title="Fade In" onPress={fadeIn} />
+            <Button title="Fade Out" onPress={fadeOut} />
         </View>
     );
 };
