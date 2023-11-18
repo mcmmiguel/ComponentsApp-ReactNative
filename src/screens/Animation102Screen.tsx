3743a9c0c2c79218787d75f2ba675561253ac142
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-17 22:10:12 -0600

    Feat. Implement AnimatedXY

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, Animated, PanResponder } from 'react-native';
 
 export const Animation102Screen = () => {
+
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event(
+            [
+                null,
+                {
+                    dx: pan.x, // x,y are Animated.Value
+                    dy: pan.y,
+                },
+            ],
+            {
+                useNativeDriver: false,
+            }
+        ),
+        onPanResponderRelease: () => {
+            Animated.spring(
+                pan, // Auto-multiplexed
+                { toValue: { x: 0, y: 0 }, useNativeDriver: false }, // Back to zero
+            ).start();
+        },
+    });
+
     return (
         <View style={styles.mainContainer}>
-            <View style={styles.purpleBox} />
+            <Animated.View
+                {...panResponder.panHandlers}
+                style={[pan.getLayout(), styles.purpleBox]}
+            />
         </View>
     );
 };
@@ -12,9 +40,11 @@ export const Animation102Screen = () => {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     purpleBox: {
-        backgroundColor: 'red',
+        backgroundColor: '#75ced8',
         width: 150,
         height: 150,
     },
