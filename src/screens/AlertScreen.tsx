bdc2e70102cbdd96a3c5634742c3cf95c414d5d7
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-18 15:54:29 -0600

    Feat. Prompt for android and ios with a package

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Alert, Button, View } from 'react-native';
+import { Alert, Button, StyleSheet, View } from 'react-native';
+import prompt from 'react-native-prompt-android';
 import { HeaderTitle } from '../components';
 import { appTheme } from '../theme/appTheme';
 
@@ -20,13 +21,31 @@ export const AlertScreen = () => {
     };
 
     const showPrompt = () => {
-        Alert.prompt('¿Está seguro?',
-            'Esta acción es irreversible',
-            (value: string) => console.log('info', value),
-            'plain-text',
-            'Hola',
-            'number-pad'
+        // ios
+        // Alert.prompt('¿Está seguro?',
+        //     'Esta acción es irreversible',
+        //     (value: string) => console.log('info', value),
+        //     'plain-text',
+        //     'Hola',
+        //     'number-pad'
+        // );
+
+        // Android
+        prompt(
+            'Enter password',
+            'Enter your password to claim your $1.5B in lottery winnings',
+            [
+                { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+            ],
+            {
+                type: 'secure-text',
+                cancelable: false,
+                defaultValue: 'test',
+                placeholder: 'placeholder',
+            }
         );
+
     };
 
     return (
@@ -38,11 +57,19 @@ export const AlertScreen = () => {
                 onPress={showAlert}
             />
 
+            <View style={styles.separator} />
+
             {/* Solo funciona en iOS */}
             <Button
-                title="Mostrar prompt (solo iOS)"
+                title="Mostrar prompt"
                 onPress={showPrompt}
             />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    separator: {
+        height: 10,
+    },
+});
