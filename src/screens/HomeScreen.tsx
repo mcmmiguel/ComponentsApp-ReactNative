849e09665789d7858bd440ed703d14d9f12cfad6
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-17 19:30:26 -0600

    Feat. Flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+interface MenuItemProps {
+    name: string;
+    icon: string;
+    component: string;
+}
+
+const menuItems = [
+    {
+        name: 'Animation 101',
+        icon: 'cube-outline',
+        component: 'Animation101Screen',
+    },
+];
 
 export const HomeScreen = () => {
+
+    const renderMenuItem = (menuItem: MenuItemProps) => {
+        return (
+            <View>
+                <Text>{menuItem.name} - {menuItem.icon}</Text>
+            </View>
+        );
+    };
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={styles.mainContainer}>
+            <FlatList
+                data={menuItems}
+                renderItem={({ item }) => renderMenuItem(item)}
+                keyExtractor={(item) => item.name}
+            />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+    },
+});
